3e8295ecafcd2cf1a21d5a2fedc624d4166220e1
eripal <erik.paledal@gmail.com>
2016-03-06 21:25:51 +0100

    InfoInAllQuarters

--- Raspberry/html/index.php
+++ Raspberry/html/index.php
@@ -50,90 +50,119 @@
 <body>
 	<div id="wrapper">
 		<div id="upper-left">
-			<div id="clock"></div> <!-- Including the date/time-script -->
-        	<div id="whatweather"></div>
+			<div id="clock"></div> <!-- Including the date/time-script -->        	
 		</div>
-	<div id="upper-right">
-		<?php 
-			$xml_data ='<REQUEST>' .
-                '<LOGIN authenticationkey="fe898a06841443beb05ea563a1ff2b8c" />' .
-                '<QUERY objecttype="TrainAnnouncement" orderby="AdvertisedTimeAtLocation">' .
-                '<FILTER>' .
-                '<AND>' .
-                '<EQ name="ActivityType" value="Avgang" />' .
-                '<EQ name="LocationSignature" value="Nkv" />' .
-                '<EQ name="ToLocation" value="Cst" />' .
-                '<OR>' .
-                '<AND>' .
-                '<GT name="AdvertisedTimeAtLocation" value="$dateadd(-00:15:00)" />' .
-                '<LT name="AdvertisedTimeAtLocation" value="$dateadd(15:00:00)" />' .
-                '</AND>' .
-                '<AND>' .
-                    '<LT name="AdvertisedTimeAtLocation" value="$dateadd(00:30:00)" />' .
-                    '<GT name="EstimatedTimeAtLocation" value="$dateadd(-00:15:00)" />' .
-                '</AND>' .
-                '</OR>' .
-                '</AND>' .
-                '</FILTER>' .
-                '<INCLUDE>AdvertisedTrainIdent</INCLUDE>' .
-                '<INCLUDE>AdvertisedTimeAtLocation</INCLUDE>' .
-                '<INCLUDE>EstimatedTimeAtLocation</INCLUDE>' .
-                '<INCLUDE>EstimatedTimeIsPreliminary</INCLUDE>' .
-                '<INCLUDE>ToLocation</INCLUDE>' .
-                '<INCLUDE>Canceled</INCLUDE>' .
-                '</QUERY>' .
-                '</REQUEST>';
-
-
-			$URL = "http://api.trafikinfo.trafikverket.se/v1/data.xml";
- 
-			$ch = curl_init($URL);
-			curl_setopt($ch, CURLOPT_MUTE, 1);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_HEADER, false);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
-			curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml_data");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$output = curl_exec($ch);
-			curl_close($ch);
-		
-//			print htmlspecialchars($output);
-			$oXML = new SimpleXMLElement($output);?>
-			<table>
-			<?php foreach($oXML->RESULT->TrainAnnouncement as $res):
-				$info = "";
-				$estTimeParts = "";
-				$time = explode("T", $res->AdvertisedTimeAtLocation);
-				$timeParts = explode(":", $time[1]);
-				if (strlen($res->EstimatedTimeAtLocation) > 0) {
-	                $info = "Delayed";
-                    $estTime = $res->EstimatedTimeAtLocation;
-					$estTime = explode("T", $res->EstimatedTimeAtLocation);
-					$estTimeParts = explode(":", $estTime[1]);
-					$est = $estTimeParts[0] . ":" . $estTimeParts[1];
-                }
-				if ($res->EstimatedTimeIsPreliminary == "true") {
-					$info = "Preliminary";
+		<div id="upper-right">  	
+			<?php 
+                $xml_data ='<REQUEST>' .
+                    '<LOGIN authenticationkey="fe898a06841443beb05ea563a1ff2b8c" />' .
+                    '<QUERY objecttype="TrainAnnouncement" orderby="AdvertisedTimeAtLocation">' .
+                    '<FILTER>' .
+                    '<AND>' .
+                    '<EQ name="ActivityType" value="Avgang" />' .
+                    '<EQ name="LocationSignature" value="Nkv" />' .
+                    '<EQ name="ToLocation" value="Cst" />' .
+                    '<OR>' .
+                    '<AND>' .
+                    '<GT name="AdvertisedTimeAtLocation" value="$dateadd(-00:15:00)" />' .
+                    '<LT name="AdvertisedTimeAtLocation" value="$dateadd(15:00:00)" />' .
+                    '</AND>' .
+                    '<AND>' .
+                        '<LT name="AdvertisedTimeAtLocation" value="$dateadd(00:30:00)" />' .
+                        '<GT name="EstimatedTimeAtLocation" value="$dateadd(-00:15:00)" />' .
+                    '</AND>' .
+                    '</OR>' .
+                    '</AND>' .
+                    '</FILTER>' .
+                    '<INCLUDE>AdvertisedTrainIdent</INCLUDE>' .
+                    '<INCLUDE>AdvertisedTimeAtLocation</INCLUDE>' .
+                    '<INCLUDE>EstimatedTimeAtLocation</INCLUDE>' .
+                    '<INCLUDE>EstimatedTimeIsPreliminary</INCLUDE>' .
+                    '<INCLUDE>ToLocation</INCLUDE>' .
+                    '<INCLUDE>Canceled</INCLUDE>' .
+                    '</QUERY>' .
+                    '</REQUEST>';
+    
+    
+                $URL = "http://api.trafikinfo.trafikverket.se/v1/data.xml";
+     
+                $ch = curl_init($URL);
+                curl_setopt($ch, CURLOPT_MUTE, 1);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_HEADER, false);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+                curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml_data");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                $output = curl_exec($ch);
+                curl_close($ch);
+            
+    //			print htmlspecialchars($output);
+                $oXML = new SimpleXMLElement($output);?>
+                <table>
+                <?php foreach($oXML->RESULT->TrainAnnouncement as $res):
+                    $info = "";
+                    $est = "";
+                    $estTimeParts = "";
+                    $time = explode("T", $res->AdvertisedTimeAtLocation);
+                    $timeParts = explode(":", $time[1]);
+                    if (strlen($res->EstimatedTimeAtLocation) > 0) {
+                        $info = "Delayed";
+                        $estTime = $res->EstimatedTimeAtLocation;
+                        $estTime = explode("T", $res->EstimatedTimeAtLocation);
+                        $estTimeParts = explode(":", $estTime[1]);
+                        $est = $estTimeParts[0] . ":" . $estTimeParts[1];
+                    }
+                    if ($res->EstimatedTimeIsPreliminary == "true") {
+                        $info = "Preliminary";
+                    }
+                    if ($res->Canceled == "true") {
+                        $info = "Canceled";
+                    }
+                    ?>           
+                        <tr>                    
+                            <td ><?php echo $info;?></td>
+                            <td ><?php echo $est;?></td>
+                            <td ><?php echo $res->AdvertisedTrainIdent;?></td>
+                            <td><?php echo $timeParts[0] . ":" . $timeParts[1]; ?></td>
+                        </tr>
+            <?php endforeach;?>
+                </table>
+        </div>
+        <div id="bottom-right">
+            <div id="whatweather"></div>
+        </div>
+        <div id="bottom-left">
+			<?php // Code for getting the RSS-news-feed
+			$rss = new DOMDocument();
+			$rss->load('http://feeds.idg.se/idg/vzzs'); // Specify the address to the feed
+			$feed = array();
+				foreach ($rss->getElementsByTagName('item') as $node) {
+					$item = array (
+					'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+					'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+					'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
+					);
+				array_push($feed, $item);
 				}
-				if ($res->Canceled == "true") {
-					$info = "Canceled";
-				}
-				?>           
-        			<tr>                    
-                    	<td ><?php echo $info;?></td>
-                        <td ><?php echo $est;?></td>
-                        <td ><?php echo $res->AdvertisedTrainIdent;?></td>
-		            	<td><?php echo $timeParts[0] . ":" . $timeParts[1]; ?></td>
-        			</tr>
-		<?php endforeach;?>
-        	</table>
-	</div>
-</div>
+   
+			$limit = 1; // Number of posts to be displayed
+			for($x=0;$x<$limit;$x++) {
+				$title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
+				$description = $feed[$x]['desc'];
+				$date = date('j F', strtotime($feed[$x]['date']));
+				echo '<h2 class="smaller">'.$title.'</h2>';
+				echo '<p class="date">'.$date.'</p>';
+				echo '<p>'.strip_tags($description, '<p><b>');
+			}
+			?>
+        </div>
+    </div>
 </body>
 </html>
-
+<script type="text/javascript" src="js/news/news.js"></script>
+<script type="text/javascript" src="js/compliments/compliments.js"></script>
 <script type="text/javascript" src="js/jquery-2.2.0.min.js"></script>
 <script type="text/javascript" src="js/mustache.js"></script>
 <script type="text/javascript" src="js/whatweather-1.2.js"></script>
